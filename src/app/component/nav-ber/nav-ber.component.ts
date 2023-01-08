5d0fbad98bb687b389347b8de8563ae727569234
Sudip Das <sudip5428@gmail.com>
2023-01-08 18:27:14 +0530

    navber add

---
+++ src/app/component/nav-ber/nav-ber.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-ber',
+  templateUrl: './nav-ber.component.html',
+  styleUrls: ['./nav-ber.component.scss']
+})
+export class NavBerComponent {
+
+}
